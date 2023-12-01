43bc62c9ebc604921704f15c83100487d877b65a
nancyy24 <93393288+nancyy24@users.noreply.github.com>
2023-12-01 23:43:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const cors = require("cors");
 const apirouting = require("../backend/Routes/Apiroutes");
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = 5000 ;
 app.use(cors()); 
 
 
