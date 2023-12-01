2a6dd76efd27c56fb012f49184a2e9ba25cbb4ba
Nancy <guptanancy2403@gmail.com>
2023-12-01 23:50:02 +0530

    changes

--- backend/Routes/Apiroutes.js
+++ backend/Routes/Apiroutes.js
@@ -3,6 +3,10 @@ const router = express.Router()
 
 const all_logs = require("../Controllers/logsController")
 
+router.get('/', (req, res) => {
+    // Send a simple HTML response
+    res.send('<h1>Hello, World!</h1>');
+  });
 router.get("/api/all_logs",all_logs.get_all_logs_data);
 
 router.post("/api/filter",all_logs.filterData);
